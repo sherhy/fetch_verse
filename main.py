a140e754545c1cad6803513b7a583529110ffa08
hyunjin <hyunjin.jeong@toki.waseda.jp>
2022-06-29 12:01:52 +0000

    feat: setup service

--- main.py
+++ main.py
@@ -1,22 +1,64 @@
-from drama_jp import DramaJP
-from drama_kr import DramaKR
-from consts import BOOKS_JP, BOOKS_KR
-from mongo import Mongo
+import tornado
+import tornado.ioloop
+from tornado.web import Application, RequestHandler, url
 
-def main(book, chapter, verse, verse_until = None):
+from utils.drama_jp import DramaJP
+from utils.drama_kr import DramaKR
+from utils.mongo import Mongo
+
+plugins = []
+
+
+def main():
+    PORT = 8004
+    app = Application([url(r"^/api/bible", FetchHandler)])
+    app.listen(PORT)
     Mongo.init_mongo()
+    load_plugins()
+    print(f"Service started on port: {PORT}")
+    tornado.ioloop.IOLoop.current().start()
+
+
+def load_plugins():
+    global plugins
     dkr = DramaKR()
     djp = DramaJP()
     plugins = [dkr, djp]
+
+
+class FetchHandler(RequestHandler):
+    def get(self):
+        try:
+            book = int(self.get_argument("book"))
+            chapter = int(self.get_argument("chapter"))
+            verse = int(self.get_argument("verse"))
+            verse_until = int(self.get_argument("verseUntil", "0"))
+        except ValueError:
+            self.set_status(400)
+            self.finish()
+
+        verses = "\n".join(fetch_verses(book, chapter, verse, verse_until))
+        self.finish({"text": verses})
+
+
+def fetch_verses(book, chapter, verse, verse_until=None):
+    global plugins
     if not verse_until:
         verse_until = verse
-    for v in range(verse, verse_until+1):
+    for v in range(verse, verse_until + 1):
         for plugin in plugins:
             text = plugin.get_pretty_verse(book, chapter, v)
-            print(text)
+            yield text
+
 
 if __name__ == "__main__":
-    import sys
-    args = map(int, sys.argv[1:])
-    main(*args)
-
+    # Mongo.init_mongo()
+    # load_plugins()
+    # book = 1
+    # chapter = 1
+    # verse = 1
+    # verse_until = None
+    # verses = '\n'.join(fetch_verses(book, chapter, verse, verse_until))
+    # v = tornado.escape.to_unicode(verses) # just have to echo -e `curl $URL`
+    # print(v)
+    main()
